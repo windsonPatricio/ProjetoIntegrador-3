9eea9cd6921b67aab04a80c6216e1763d70561a8
wilsonPMedeiros <wilson.patricio@escolar.ifrn.edu.br>
2021-04-24 20:36:05 -0300

    teste de erro do heroku 2

--- src/main/java/com/ifrn/sisconpro/security/WebSecurityConfig.java
+++ src/main/java/com/ifrn/sisconpro/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/login").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin()
                         .loginPage("/login")
